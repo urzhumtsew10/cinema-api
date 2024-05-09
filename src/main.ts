7697f92b7368bb46ed1d66ad85ff6219cddac822
urgumandrei <urgumandrei@gmail.com>
2024-05-09 11:10:31 +0300

    add method OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE OPTIONS',
     credentials: true,
   });
 

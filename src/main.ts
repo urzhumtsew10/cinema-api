858934b100e420a96136c039d65d229bc0da9a31
urgumandrei <urgumandrei@gmail.com>
2024-05-09 10:58:24 +0300

    cors error

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,11 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
